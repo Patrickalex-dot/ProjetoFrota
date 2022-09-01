93f251f02cf9f1cd80e1dc31aa667a25d5aae5e9
patrick alexandre <palexandre@live.com>
2022-08-31 22:14:56 -0300

    repository do caminhão concluido

--- ProjetoFrota/Repositorys/CaminhaoRepository.cs
+++ ProjetoFrota/Repositorys/CaminhaoRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Dapper;
 using ProjetoFrota.Dto_s;
 using ProjetoFrota.ViewModelAtualizar;
+using ProjetoFrota.ViewModelSalvar;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
@@ -12,7 +13,7 @@ namespace ProjetoFrota.Repositorys
     {
         private readonly string Conexao = @"Data Source=ITELABD14\SQLEXPRESS;Initial Catalog=ProjetoFrota;Integrated Security=True;";
 
-        public bool Salvar(CadastrarCaminhaoViewModel cadastrarCaminhao)
+        public bool Salvar(SalvarCaminhaoViewModel cadastrarCaminhao)
         {
             try
             {
@@ -58,6 +59,85 @@ namespace ProjetoFrota.Repositorys
             {
                 Console.WriteLine("Erro:" + execao.Message);
                 return null;
+            }
+
+        }
+        public List<CaminhaoDto> BuscarTodos()
+        {
+            List<CaminhaoDto> caminhaoEncontrado;
+            try
+            {
+                var query = @"SELECT CaminhaoId,Modelo,Placa FROM Caminhao";
+                using (var connection = new SqlConnection(Conexao))
+                {
+                    caminhaoEncontrado = connection.Query<CaminhaoDto>(query).ToList();
+                }
+                return caminhaoEncontrado;
+            }
+            catch (Exception execao)
+            {
+                Console.WriteLine("Erro:" + execao.Message);
+                return null;
+            }
+        }
+        public void Atualizar (AtualizarCaminhaoViewModel caminhao)
+        {
+            try
+            {
+                var query = @"UPDATE Caminhao SET Modelo = @modelo, Placa = @placa";
+                using (var sql = new SqlConnection(Conexao))
+                {
+                    SqlCommand command = new SqlCommand(query,sql);
+                    command.Parameters.AddWithValue("@modelo", caminhao.Modelo);
+                    command.Parameters.AddWithValue("@placa", caminhao.Placa);
+                    command.Connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception execao)
+            {
+                Console.WriteLine("Erro" + execao.Message);
+            }
+        }
+        public CaminhaoDto Confirmar(int CaminhaoId)
+        {
+            var caminhao = new CaminhaoDto();
+            try
+            {
+                var query = "SELECT * FROM Caminhao WHERE CaminhaoId = @caminhaoId";
+                using (var  connection = new SqlConnection(Conexao))
+                {
+                    var parametros = new
+                    {
+                        CaminhaoId
+                    };
+                    caminhao = connection.QueryFirstOrDefault<CaminhaoDto>(query, parametros);
+                }
+            }
+            catch (Exception execao)
+            {
+                Console.WriteLine("Erro: " + execao.Message);
+                caminhao = null;
+            }
+            return caminhao;
+
+        }
+        public void Deletar (int Id)
+        {
+            try
+            {
+                var query = "DELETE FROM Caminhao WHERE CaminhaoId = @Id";
+                using (var sql = new SqlConnection(Conexao))
+                {
+                    SqlCommand command = new SqlCommand(query, sql);
+                    command.Parameters.AddWithValue("@Id", Id);
+                    command.Connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception execao)
+            {
+                Console.WriteLine("Erro: " + execao.Message);
             }
         }
     }
--- ProjetoFrota/Repositorys/MotoristaRepository.cs
+++ ProjetoFrota/Repositorys/MotoristaRepository.cs
@@ -1,10 +1,20 @@
-﻿namespace ProjetoFrota.Repositorys
+﻿using ProjetoFrota.ViewModelSalvar;
+
+namespace ProjetoFrota.Repositorys
 {
     public class MotoristaRepository
     {
-        public string Nome { get; set; }
-        public string Endereco { get; set; }
-        public int Id { get; set; }
-        public string Cpf { get; set; }
+        private readonly string Conexao = @"Data Source=ITELABD14\SQLEXPRESS;Initial Catalog=ProjetoFrota;Integrated Security=True;";
+
+        public bool Salvar (SalvarMotoristaViewModel salvarMotorista)
+        {
+            try
+            {
+                var query = @"INSERT INTO Motorista 
+                            (Nome,Endereco, Cpf)
+                            VALUES (@nome,@endereco,@cpf) ";
+
+            }
+        }
     }
 }
